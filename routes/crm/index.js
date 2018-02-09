efaf9bbd2f28ae94fd995b7cd4cf36ac22154731
densk1 <>
2018-02-09 12:42:06 +0000

    Fixed Conflict: Created a searchable route for partial strings

--- routes/crm/index.js
+++ routes/crm/index.js
@@ -7,15 +7,10 @@ const mongoose = require('mongoose');
 const Client = mongoose.model('clients');
 
 module.exports = (passport) => {
-
-	
-	router.post("/create", passport.authenticate(), (req, res) => {
-        
-		res.json({ status: "TEST: My API is alive!" });
-	});
-	router.get('/list', passport.authenticate(), (req, res) => {
-        console.log(req.user);
-        console.log(req.params);
+    router.use(passport.authenticate());
+    
+	router.post("/create", (req, res) => {
+/*
         new Client({
             ownerID: 1,
             name: 'Carden',
@@ -23,15 +18,25 @@ module.exports = (passport) => {
             company: 'indiepics',
             email: 'carden.k@indiepics.ie', 
         }).save();
+*/
+		res.status(200).json({ result: "TEST: My API is alive!" });
+	});
+	router.get('/list/:clientID', (req, res) => {
+        console.log(req.user);
+        console.log(req.params);
+
         return res.status(200).json({result: "List Route"});
 	});
 	
-	router.all('/search', async (req, res) => {
-		res.status(200).send(await Client.find());
-		// pagination methods
-		//.skip(10));
-		//.limit(2));
-	});
+    router.post('/search', async (req, res) => {
+        console.log(req.body.query);
+        const result = await Client.find({
+            name: new RegExp(".*" + req.body.query+ ".*")
+        });
+        // const result = Client.find({name: req.body });
+        console.log(result);
+        res.status(200).json({});
+    });
 	
 	return router;
 }
