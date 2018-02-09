3838dc427d03ec5861317a6c1e2eb18681962b4d
densk1 <>
2018-02-09 17:45:56 +0000

    Created response for /crm/list route

--- routes/crm/index.js
+++ routes/crm/index.js
@@ -9,27 +9,31 @@ const Client = mongoose.model('clients');
 module.exports = (passport) => {
     router.use(passport.authenticate());
     
-	router.post("/create", (req, res) => {
-/*
-        new Client({
-            ownerID: 1,
-            name: 'Carden',
-            job: 'Digital Producer',
-            company: 'indiepics',
-            email: 'carden.k@indiepics.ie', 
-        }).save();
-*/
-		res.status(200).json({ result: "TEST: My API is alive!" });
-	});
-	router.get('/list/:clientID', (req, res) => {
-        console.log(req.user);
-        console.log(req.params);
 
+    router.post('/list', (req, res) => {
+        let offset = req.body.offset || 3;
+        let amount = req.body.amount || 5;
+        Client.find().skip(offset).limit(amount)
+        .then( (result) => {
+            res.status(200).json(result);
+        }).catch( (err) => {
+            res.status(500).end();
+        });
+    });
+
+    
+	router.post('/client/:clientID', (req, res) => {
+        // boostrap Card!
+        console.log(req.user, req.params);
+        return res.status(200).json({result: "/client/:clientID Not yet Complete!"});
+	});
+	router.post('/client/:clientID/edit', (req, res) => {
+        console.log(req.user, req.params, req.body);
         return res.status(200).json({result: "List Route"});
 	});
 	
     router.post('/search', async (req, res) => {
-        console.log(req.body.query);
+        console.log(req.body);
         const result = await Client.find({
             name: new RegExp(".*" + req.body.query+ ".*")
         });
@@ -37,6 +41,21 @@ module.exports = (passport) => {
         console.log(result);
         res.status(200).json({});
     });
-	
+	router.post("/create", (req, res) => {
+        /*
+        new Client({
+            ownerID: 1,
+            name: 'Carden',
+            job: 'Digital Producer',
+            company: 'indiepics',
+            email: 'carden.k@indiepics.ie', 
+        }).save();
+        */
+		res.status(200).json({ result: "/Create route note yet built" });
+	});
+    
+    router.post('/list', (req, res) => {
+        res.status(200).json({result: '/crm/list Not yet complete'});
+    })
 	return router;
 }
